97bebbd47e1090a1ddd71d1f440178a90f4e6135
FabioTM <miyasato.fabio@gmail.com>
2018-07-24 18:57:44 -0300

    setup total

--- app/src/main/java/com/digitalhouse/workshop/MainActivity.java
+++ app/src/main/java/com/digitalhouse/workshop/MainActivity.java
@@ -4,14 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton
+        .OnCheckedChangeListener {
 
     private CheckBox item1;
     private CheckBox item2;
     private CheckBox item3;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         item1 = findViewById(R.id.checkBox);
         item2 = findViewById(R.id.checkBox2);
         item3 = findViewById(R.id.checkBox3);
+        totalTextView = findViewById(R.id.textViewTotal);
 
         ImageView imagem = findViewById(R.id.imageView);
         imagem.setAlpha(0F);
@@ -29,34 +34,47 @@ public class MainActivity extends AppCompatActivity {
                 .setDuration(5000)
                 .start();
 
+        item1.setOnCheckedChangeListener(this);
+        item2.setOnCheckedChangeListener(this);
+        item3.setOnCheckedChangeListener(this);
+
     }
 
     public void finalizarPedido(View view) {
-
-        // Totalizador de valor
-        float total = 0F;
-
-        // Verifica se o item esta preenchido
-        if(item1.isChecked()){
-            // Sumariza o valor do item 1
-            total += Float.parseFloat(item1.getText().toString());
-        }
-        if(item2.isChecked()){
-            total += Float.parseFloat(item2.getText().toString());
-        }
-        if(item3.isChecked()){
-            total += Float.parseFloat(item3.getText().toString());
-        }
+        float total = getTotal();
 
         String mensagem;
         // Exibe diferentes mensagens de acordo com criterio
-        if(total == 0){
+        if (total == 0) {
             mensagem = "Por favor, selecione pelo menos um item!";
-        }else{
+        } else {
             mensagem = "Valor total do Pedido: R$" + total;
         }
 
         // Exibe mensagem
         Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        totalTextView.setText("Total do Pedido: R$" + getTotal());
+    }
+
+    private float getTotal() {
+        // Totalizador de valor
+        float total = 0F;
+
+        // Verifica se o item esta preenchido
+        if (item1.isChecked()) {
+            // Sumariza o valor do item 1
+            total += Float.parseFloat(item1.getText().toString());
+        }
+        if (item2.isChecked()) {
+            total += Float.parseFloat(item2.getText().toString());
+        }
+        if (item3.isChecked()) {
+            total += Float.parseFloat(item3.getText().toString());
+        }
+        return total;
     }
 }
